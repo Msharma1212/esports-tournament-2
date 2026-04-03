cca57fedb234c219fa3a82aaafcd427098c1b483
Msharma1212 <kmayank122004@gmail.com>
2026-04-03 16:35:09 +0530

    Speed Insights component added

--- website/src/app/(main)/layout.tsx
+++ website/src/app/(main)/layout.tsx
@@ -1,5 +1,5 @@
 import { Header, Footer } from "@/components/layout";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export default function MainLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
